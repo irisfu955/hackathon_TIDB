c27a1845d980a3311ef686fd7e9f373046dd95cd
irisfu955 <irisfu955@gmail.com>
2025-10-21 11:32:35 -0400

    change database

--- hackathon_tidb/src/auth.ts
+++ hackathon_tidb/src/auth.ts
@@ -2,7 +2,10 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  })],
   callbacks: {
     session: async ({ session, token }) => {
       if (session?.user && token?.sub) {
